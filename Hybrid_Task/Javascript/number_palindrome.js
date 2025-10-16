e701f4c7e947a814a5be078b3f1f023e9b76e685
eniife <eniifeoluwaadeniyi16@gmail.com>
2025-10-16 02:21:23 +0100

    Add string_palindrome.js

--- Hybrid_Task/Javascript/number_palindrome.js
+++ Hybrid_Task/Javascript/number_palindrome.js
@@ -1,9 +1,8 @@
-let number = "12345"
+let number = "12321"
 
 let reverse = " "
 for(let digit = 0; digit < number.length; digit += 1) {
 	 reverse = number[digit] + reverse
 }
-console.log(reverse);
 if(reverse == Number(number)) console.log(number + " is a palindrome!")
 if(reverse != Number(number)) console.log(number + " is not a palindrome!")
