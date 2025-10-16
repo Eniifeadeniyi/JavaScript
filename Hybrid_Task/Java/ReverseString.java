2fbf65c5cad620a9f8a29d377acf96857fdf9cef
eniife <eniifeoluwaadeniyi16@gmail.com>
2025-10-16 03:30:33 +0100

    ReverseString.java

--- Hybrid_Task/Java/ReverseString.java
+++ Hybrid_Task/Java/ReverseString.java
@@ -3,12 +3,11 @@ public static void main(String[] args) {
 
 String word = "eniife";
 
-String[] words = word.split(" ");
 
 String reverse = " ";
-System.out.print(word.length());
+
 for(int index = 0; index < word.length(); index++) {
-reverse += words[index];	
+reverse = word.charAt(index) + reverse;	
 }
 
 System.out.print(reverse);
