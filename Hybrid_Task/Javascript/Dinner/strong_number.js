d211e4908557e5a529b98bb5499274060d876047
eniife <eniifeoluwaadeniyi16@gmail.com>
2025-10-19 07:59:04 +0100

    Add strong_number.js

--- Hybrid_Task/Javascript/Dinner/strong_number.js
+++ Hybrid_Task/Javascript/Dinner/strong_number.js
@@ -2,29 +2,14 @@ let prompt = require('prompt-sync')();
 
 let number = prompt("Enter a number: ");
 
-let divisor = 10 ** (number.length - 1)
-const range = number.length
-let digits = ""
-for(let count = 0; count < range; count++) {
-	let digit = Math.floor(number / divisor);
-	digits += digit;
-	number %= divisor; 
-	divisor /= 10;
-	}
-
-console.log(digits)
-
-let sum = 0;
-let answer = 1;
-for(let index = 0; index < digits.length; index++) {
-for(digit.charAt(index)) {
-	//console.log(digit)
-	for(let count = 1; count <= digit; count++) {
-	answer *= count;
-	console.log(answer)
+let sum = 0
+for(digit in String(number)){
+	let answer = 1
+	for(let count = 1; count <= number.charAt(digit); count++){
+		answer *= count
 	
 }
 sum += answer
-console.log(sum)
 }
-}
+if(sum == number) console.log(number + " is a strong number!")
+else console.log(number + " is not a strong number!")
