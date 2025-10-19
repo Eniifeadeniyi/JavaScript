71132394be1c15fa28c627e72aaa8bd1e9b487e3
eniife <eniifeoluwaadeniyi16@gmail.com>
2025-10-19 14:49:27 +0100

    Add StrongNumber.java

---
+++ Hybrid_Task/Java/Dinner/StrongNumber.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class StrongNumber {
+public static void main(String[] args) {
+Scanner input = new Scanner(System.in);
+
+System.out.print("Enter a number: ");
+int number = input.nextInt();
+
+String num = Integer.toString(number);
+	int sum = 0;
+for(int count = 0; count < num.length(); count++) {
+	int answer = 1;
+	char digits = num.charAt(count);
+	int digit = Character.getNumericValue(digits);
+	for(int counter = 1; counter <= digit; counter++) {
+		answer *= counter;
+		
+}
+sum += answer;
+}
+if(sum == number) System.out.print(number + " is a strong number!");
+else System.out.print(number + " is not a strong number!");
+
+}
+}
