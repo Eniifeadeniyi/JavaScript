fa5c26429a8d06573aa33720bf5b1644cb069615
eniife <eniifeoluwaadeniyi16@gmail.com>
2025-10-19 14:51:40 +0100

    Add ArmstrongNumbers.java

--- Hybrid_Task/Java/Dinner/ArmstrongNumbers.java
+++ Hybrid_Task/Java/Dinner/ArmstrongNumbers.java
@@ -8,7 +8,7 @@ for(int number = 1; number < 1001; number++) {
 	for(int count = 0; count < num.length(); count++) {
 		char digits = num.charAt(count);
 		int digit = Character.getNumericValue(digits);
-		Double power = Math.pow(digit, 3);
+		Double power = Math.pow(digit, num.length());
 		sum += power;
 }
 if(sum == number) System.out.println(number);
