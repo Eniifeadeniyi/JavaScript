1080315af429a3f47c476e3a51c1e9451b770cfd
eniife <eniifeoluwaadeniyi16@gmail.com>
2025-11-24 03:48:30 +0100

    test

--- Hybrid_Task/Java/CarFuelSystem/TestFunctions.java
+++ Hybrid_Task/Java/CarFuelSystem/TestFunctions.java
@@ -4,10 +4,49 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class TestFunctions {
 
 	@Test
-	public void testThatStartCarWorks(){
+	public void testThatCarCanStart(){
 		Car car = new Car(20.2);
 		car.startCar();
 		assertEquals(car.getStartCar(),true);
+
+}
+
+	@Test
+	public void testThatCarCanStop(){
+		Car car = new Car(20.2);
+		car.stopCar();
+		assertEquals(car.getStartCar(),false);
+
+}
+
+	@Test
+	public void testThatCarCanMove(){
+		Car car = new Car(20.2);
+		car.moveCar(50);
+		assertEquals(car.getDistance(),50.0);
+}
+
+	@Test
+	public void testThatCarFuelReducesWhenCarMoves(){
+		Car car = new Car(20);
+		car.addToFuel(50);
+		car.moveCar(50);
+		assertEquals(car.getCurrentFuelLevel(),47.5);
+}
+
+	@Test
+	public void testThatYouCanAddToFuel(){
+		Car car = new Car(20.2);
+		car.addToFuel(45);
+		assertEquals(car.getCurrentFuelLevel(),45.0);
+
+}
+
+	@Test
+	public void testThatAddToFuelDoesNotTakeInvalidInput(){
+		Car car = new Car(20.2);
+		car.addToFuel(-20);
+		assertEquals(car.getCurrentFuelLevel(),0.0);
 
 }
 
