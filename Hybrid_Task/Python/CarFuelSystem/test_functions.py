3de922ded12e231cc9c816a18768313e141316d2
eniife <eniifeoluwaadeniyi16@gmail.com>
2025-11-24 05:58:15 +0100

    test

--- Hybrid_Task/Python/CarFuelSystem/test_functions.py
+++ Hybrid_Task/Python/CarFuelSystem/test_functions.py
@@ -9,6 +9,20 @@ class Test_Functions(unittest.TestCase):
 		self.assertEqual(stop_car(), False)
 
 	def test_that_set_maximum_distance_works(self):
-		self.assertEqual(set_maximum_distance(20), )
+		self.assertEqual(set_maximum_distance(20),1000)
+
+	def test_that_set_maximum_distance_doesnt_take_invalid_input(self):
+		self.assertEqual(set_maximum_distance(-20), 0.0)
+
+	def test_that_add_fuel_works(self):
+		self.assertEqual(add_fuel(0.0,50), 50)
+
+	def test_that_add_fuel_doesnt_take_invalid_input(self):
+		self.assertEqual(add_fuel(0.0,-20),0.0)
 	
+	def test_that_add_fuel_doesnt_increase_pass_fifty(self):
+		self.assertEqual(add_fuel(47.5,4),47.5)
+
+	def test_that_move_car_works(self):
+		self.assertEqual(move_car(50,1000,50,20,0), (50,47.5))
 
