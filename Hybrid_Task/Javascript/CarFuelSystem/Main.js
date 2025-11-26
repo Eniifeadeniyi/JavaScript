fd76c2ddb05911a12227cad6ecb6ec01b8f6f7ce
eniife <eniifeoluwaadeniyi16@gmail.com>
2025-11-26 15:41:56 +0100

    ...

--- Hybrid_Task/Javascript/CarFuelSystem/Main.js
+++ Hybrid_Task/Javascript/CarFuelSystem/Main.js
@@ -1,65 +1,70 @@
-const {moveCar,addFuel,startCar,stopCar, setMaximumDistance,validate} = require("./Functions")
-
 const prompt = require('prompt-sync')();
-let efficiency = prompt('Enter fuel efficiency: ');
-while(!(validate(efficiency))) {
-    console.log("Invalid input!")
-    efficiency = prompt('Enter fuel efficiency: ');
+const {moveCar,addFuel,startCar,stopCar,setMaximumDistance,validate} = require("./Functions");
+let efficiency = prompt("Enter fuel efficiency: ");
+
+while(!(validate(efficiency))){
+    console.log("Invalid input");
+    efficiency = prompt("Enter fuel efficiency: ");
 }
-efficiency = Number(efficiency);
-setFuelEfficiency(efficiency);
-setMaximumDistance();
+
+efficiency = Number(efficiency)
+let maximum = setMaximumDistance(efficiency)
+let distanceMoved = 0.0
+let start = false
+let totalFuel = 0.0
 
 let menu = `
     1. Start Car
-    2. Move Car
-	3. Check Fuel
-	4. Increase Fuel
-	5. Stop Car
+    2. Check Fuel level
+    3. Add fuel
+    4. Move Car
+    5. Stop Car
 `
-let operation = "";
-while(operation != 5){
+let operation = ""
+while(operation != "5"){
     console.log(menu)
-    operation = prompt("Enter an operation: ");
+    operation = prompt("Enter your choice: ")
     switch(operation){
-        case "1": 
-            {
-                startCar()
-                break;
-            }
-        case "2":
-            {
-                if(start == true && currentFuel > 0.0){
-                let distance = prompt("Enter distance: ");
-                if(validate(distance)) {
-                    distance = Number(distance);
-                    moveCar(distance)
-                    console.log("Moved " + distance + " kilometers")
-                    console.log("Moved a total of " + getDistanceMoved() + " kilometers");
-                }
-                else console.log("Invalid input!")
-                }
-                else console.log("Start and refuel car first!");
-                break;
-            }
-        case "3":{
-            console.log(checkFuelLevel());
+        case "1": {
+            start = startCar();
             break;
-            }
-        case "4":{
+        }
+        case "2": {
+            console.log(addFuel(totalFuel));
+            break;
+        }
+        case "3": {
             let fuel = prompt("Enter fuel amount: ");
-            if(validate(fuel)){
-                fuel = Number(fuel)
-                addFuel(fuel) 
+            while(!(validate(fuel))){
+                console.log("Invalid input");
+                fuel = prompt("Enter fuel amount: ");
             }
-            break;            
-            }
-        case "5":{
-            stopCar()
+            fuel = Number(fuel);
+            totalFuel = addFuel(totalFuel,fuel);
             break;
+        }
+        case "4": {
+            if(totalFuel > 0.0 && start == true){
+                let distance = prompt("Enter distance: ")
+                while(!(validate(distance))){
+                    console.log("Invalid input");
+                    distance = prompt("Enter distance: ");
+                }
+                distance = Number(distance)
+                distanceMoved, totalFuel = moveCar(distance, maximum, totalFuel, efficiency,distanceMoved)
+                console.log("Moved " + distance + " kilometers");
+                console.log("Moved a total of " + distanceMoved + " kilometers successfully");
             }
-        default : console.log("Invalid input!")
-
+            else console.log("Start car and add fuel first!");
+            break;
+        }
+        case "5": {
+            start = stopCar();
+            break;
+        }
+        default : {
+            console.log("Invalid input");
+        }
     }
 }
 
